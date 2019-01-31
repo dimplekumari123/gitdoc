5c26551d208f142545142800c6850bed4cd9327b
Dimple <dimple.tendulkar@gmail.com>
2019-01-31 20:45:32 +0530

    modified in st.java

--- st.java
+++ st.java
@@ -1,7 +1,8 @@
 class Student{
 public static void main(String args){
 System.out.println("My name is dimple");
-System.out.println("lives in banglore")
+System.out.println("lives in banglore");
+System.out.println("i mworking");
 
 }
 }
